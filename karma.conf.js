3a40199bb0c89c5b0f03b517c39878e076fd14dd
kanhiya-sh <sharmakanhiya288@gmail.com>
2026-05-14 00:43:26 +0530

    Refactor. SonarQube Files

---
+++ karma.conf.js
@@ -0,0 +1,39 @@
+// Karma configuration for Angular + SonarCloud coverage
+module.exports = function (config) {
+  config.set({
+    basePath: '',
+    frameworks: ['jasmine', '@angular-devkit/build-angular'],
+    plugins: [
+      require('karma-jasmine'),
+      require('karma-chrome-launcher'),
+      require('karma-jasmine-html-reporter'),
+      require('karma-coverage'),
+      require('@angular-devkit/build-angular/plugins/karma')
+    ],
+    client: { clearContext: false },
+    jasmineHtmlReporter: { suppressAll: true },
+    coverageReporter: {
+      dir: require('path').join(__dirname, './coverage/flowboard-frontend'),
+      subdir: '.',
+      reporters: [
+        { type: 'html' },
+        { type: 'lcovonly' },
+        { type: 'text-summary' }
+      ]
+    },
+    reporters: ['progress', 'kjhtml'],
+    port: 9876,
+    colors: true,
+    logLevel: config.LOG_INFO,
+    autoWatch: true,
+    browsers: ['Chrome'],
+    customLaunchers: {
+      ChromeHeadlessCI: {
+        base: 'ChromeHeadless',
+        flags: ['--no-sandbox', '--disable-gpu', '--disable-dev-shm-usage']
+      }
+    },
+    singleRun: false,
+    restartOnFileChange: true
+  });
+};
